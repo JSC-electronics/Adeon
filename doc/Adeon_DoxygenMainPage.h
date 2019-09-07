878e9d50a3f2f2efeab36dad32e00ba7c06f01fd
Ales_Svoboda <ales.svoboda@jsc-electronics.cz>
2019-09-07 22:21:03 +0200

    Adeon_DoxygenMainPage.h added,  Doxyfile moved to doc folder

---
+++ doc/Adeon_DoxygenMainPage.h
@@ -0,0 +1,15 @@
+/**
+ * @mainpage Adeon Arduino Library
+ *
+ * See the documentation of the main class, Adeon, or browse the modules
+ * and examples using the sidebar on the left.
+ *
+ * The latest development version is available on [GitHub](https://github.com/JSC-electronics/Adeon).
+ */
+
+/**
+ * @example AdeonBlink.ino
+ *
+ * This example demonstrates switching a LED on or off by the incoming SMS message in the 
+ * Adeon format.
+ */
