6a3ae4eb8bc2a36ac2219d830039c39e3777274c
robert_faltus <robert@jsc-electronics.cz>
2020-05-29 12:15:48 +0200

    New eeprom functions

--- src/utility/AdeonMem.cpp
+++ src/utility/AdeonMem.cpp
@@ -5,17 +5,27 @@ AdeonMem::AdeonMem(){
 }
 
 bool AdeonMem::isConfigAvailable(){
-	for(int i = 0; i < ADMIN_RECORS_LEN; i++){
+	for(int i = 0; i < ADMIN_RECORD_LEN; i++){
 		if(EEPROM.read(IDX_ADMIN_PN + 0) == BLANK_CELL){
 			deleteDatabase();
 			return false;
 		}
 	}
+	_numOfUsers = readNumOfUsers();
 	return true;
 }
 
-uint8_t AdeonMem::searchUser(){
-	
+uint8_t AdeonMem::searchUser(const char* userPn){
+	uint8_t j, i;
+	char tmp;
+	for(i = 0; i < _numOfUsers; i++){
+		for(j = 0; j < USER_RECORD_LEN; j++){
+			tmp = EEPROM.read(IDX_DATA_PART + i * USER_RECORD_LEN + j);
+			if(tmp != userPn[j]) break;
+			if(j == USER_RECORD_LEN - 1) return IDX_DATA_PART + i * USER_RECORD_LEN;
+		}
+	}
+	return NOT_FOUND;
 }
 
 uint8_t AdeonMem::readNumOfUsers(){
@@ -28,8 +38,7 @@ char* AdeonMem::readUserRecord(uint8_t numOfUserOrder){
 	if(numOfUserOrder > MAX_NUM_OF_USERS){
 		uint8_t i = 0;
 		uint8_t idx = IDX_DATA_PART + numOfUserOrder * USER_RECORD_LEN;
-		if(_returnBuffer == nullptr) free(_returnBuffer);
-		_returnBuffer = (char*) malloc (USER_RECORD_LEN + 1);
+		setReturnBuffer(USER_RECORD_LEN);
 		for(i = 0; i < USER_RECORD_LEN; i++){
 			_returnBuffer[i] = EEPROM.read(idx + i);
 		}
@@ -40,27 +49,47 @@ char* AdeonMem::readUserRecord(uint8_t numOfUserOrder){
 }
 
 char* AdeonMem::readPin(){
-
+	uint8_t i = 0;
+	setReturnBuffer(PIN_RECORD_LEN);
+	for(i = 0; i < PIN_RECORD_LEN; i++){
+		_returnBuffer[i] = EEPROM.read(IDX_PIN + i);
+	}
+	_returnBuffer[i] = '\0';
+	return _returnBuffer;
 }
 
 char* AdeonMem::readAdminPn(){
-
+	uint8_t i = 0;
+	setReturnBuffer(ADMIN_RECORD_LEN);
+	for(i = 0; i < ADMIN_RECORD_LEN; i++){
+		_returnBuffer[i] = EEPROM.read(IDX_ADMIN_PN + i);
+	}
+	_returnBuffer[i] = '\0';
+	return _returnBuffer;
 }
 
 void AdeonMem::updatePin(const char* pin){
-
+	for(uint8_t i = 0; i < PIN_RECORD_LEN; i++){
+		EEPROM.update(IDX_PIN + i, pin[i]);
+	}
 }
 
 void AdeonMem::updateAdmin(const char* adminPn){
-
+	for(uint8_t i = 0; i < ADMIN_RECORD_LEN; i++){
+		EEPROM.update(IDX_ADMIN_PN + i, adminPn[i]);
+	}
 } 
 
 void AdeonMem::updateUsers(const char* userPn){
-
+	for(uint8_t i = 0; i < USER_RECORD_LEN; i++){
+		EEPROM.update(IDX_DATA_PART + USER_RECORD_LEN * _numOfUsers + i, userPn[i]);
+	}
+	_numOfUsers++;
+	updateNumOfUsers(_numOfUsers);
 }
 
 void AdeonMem::updateUsersRights(const char* userPn, uint8_t rights){
-
+	uint8_t idx = searchUser(userPn);
 }
 
 void AdeonMem::deleteUser(const char* userPn){
@@ -73,7 +102,12 @@ void AdeonMem::deleteDatabase(){
 	}
 }
 
-void AdeonMem::setNumOfUsers(uint8_t numOfUsers){
+void AdeonMem::updateNumOfUsers(uint8_t numOfUsers){
 	EEPROM.update(IDX_NUM_USERS, numOfUsers);
+}
+
+void AdeonMem::setReturnBuffer(uint8_t size){
+	if(_returnBuffer == nullptr) free(_returnBuffer);
+	_returnBuffer = (char*) malloc (size + 1);
 }
 
--- src/utility/AdeonMem.h
+++ src/utility/AdeonMem.h
@@ -3,12 +3,13 @@
 
 #define EEPROM_LEN			1023 //bytes
 #define BLANK_CELL			0xFF //0xFF is value which means the cell is blank
+#define NOT_FOUND			0xFF
 #define MAX_NUM_OF_USERS	77
 
 #define USER_RECORD_LEN		13 	//bytes
 #define PIN_RECORD_LEN		4	//bytes
-#define ADMIN_RECORS_LEN	12	//bytes
-#define USERS_N_RECORDS_LEN	1	//bytes
+#define ADMIN_RECORD_LEN	12	//bytes
+#define USERS_N_RECORD_LEN	1	//bytes
 
 #define IDX_DATA_PART		20 	//bytes
 #define IDX_PIN				0
@@ -19,14 +20,14 @@ class AdeonMem
 {
 	private:
 		char *_returnBuffer;
-		uint8_t _lastUserIdx = 0;
+		uint8_t _numOfUsers = 0;
 
 	public:
 		AdeonMem();
 
 		bool isConfigAvailable();
 
-		uint8_t searchUser();
+		uint8_t searchUser(const char* userPn);
 
 		uint8_t readNumOfUsers();
 		char* readUserRecord(uint8_t numOfUserOrder);
@@ -42,7 +43,8 @@ class AdeonMem
 		void deleteDatabase();
 
 	private:
-		void setNumOfUsers(uint8_t numOfUsers);
+		void updateNumOfUsers(uint8_t numOfUsers);
+		void setReturnBuffer(uint8_t size);
 
 };
 
